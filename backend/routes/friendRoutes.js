6a91ddd199730f3fdc6ee6be6e5d853edc28a558
ManishRana0725 <manishbadm0725@gmail.com>
2025-03-18 11:11:23 +0530

    updated paypage backend

--- backend/routes/friendRoutes.js
+++ backend/routes/friendRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/',  FriendContoroller.createFriend);  // Create friend & generate QR
 router.get('/qr-pdf/:name', FriendContoroller.getQrPdf);  // Get QR as PDF
 router.get("/all", Token , FriendContoroller.getAllFriends); // to get all the friends
-router.get('/:id',Token, FriendContoroller.getFriendById); // Get a friend by ID
+router.get('/:id', FriendContoroller.getFriendById); // Get a friend by ID
 
 module.exports =  router;
